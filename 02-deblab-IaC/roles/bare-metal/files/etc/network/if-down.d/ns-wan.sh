a86d15c10ff6d88575a946cedb757ff15bd9be52
Laurent Granié <laurent.granie@gmail.com>
2024-02-20 16:45:07 +0100

    [deblab-IaC] - network via systemd

--- 02-deblab-IaC/roles/bare-metal/files/etc/network/if-down.d/ns-wan.sh
+++ 02-deblab-IaC/roles/bare-metal/files/etc/network/if-down.d/ns-wan.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-if [ "$IFACE" = "enp5s0" ]; then
-    # Init netns wan
-    if [ ! -f /run/netns/wan ]; then
-        ip netns delete wan
-    fi
+# Init netns wan
+if [ ! -f /run/netns/wan ]; then
+    ip netns delete wan
 fi
